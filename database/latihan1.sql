7ec7209577c53d30e07bc1007700d0adf146e553
anandapdp <168748726+anandapdp@users.noreply.github.com>
2024-08-04 02:21:34 +0700

    Update latihan1.sql

--- database/latihan1.sql
+++ database/latihan1.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 21 Mar 2023 pada 07.34
--- Versi server: 10.4.24-MariaDB
--- Versi PHP: 7.4.29
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
